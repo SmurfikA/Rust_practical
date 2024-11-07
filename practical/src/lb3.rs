a090f3f9f5b8a93cf812906537db43909c922e42
SmurfikA <o.kozachenko@e-u.edu.ua>
2024-11-07 17:55:31 +0200

    Добавлены новые файлы в папку

--- practical/src/lb3.rs
+++ practical/src/lb3.rs
@@ -1,3 +1,4 @@
+#[test]
 fn test() {
     let (x, y);
     (x, _) = (3, 4);
--- practical/src/main.rs
+++ practical/src/main.rs
@@ -1,5 +1,9 @@
+
 mod rhombus;
 mod envelope;
+mod gcd;
+mod chapter3;
+mod lb3;
 
 fn main() {
     println!("Hello, world!");
